5f2236f4b65fdac5fccfcaee505a3f3d04be30e0
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-07-02 22:28:13 +0500

    .

--- controller/authcontroller.js
+++ controller/authcontroller.js
@@ -143,10 +143,10 @@ const AuthControllerFunctions = {
         name: verifiedUser.name,
         role: verifiedUser.role,
       });
-
       await res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: true,
+        sameSite: "None",
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
 
