60d09f1156e2f311168f1e47e875e6d44f52ce6e
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-07-02 21:37:28 +0500

    cors changed

--- server.js
+++ server.js
@@ -28,7 +28,17 @@ const app = express();
 //     }
 //   })
 // );
-app.use(cors());
+// app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.use(express.json());
 app.use(cookieParser());
 app.use("/fitness-den", authRouter);
