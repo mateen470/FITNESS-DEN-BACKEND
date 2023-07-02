ffb82bbc2e22edf18a1de4f7ef1a6e2201bce878
Abdul Mateen <100369328+mateen470@users.noreply.github.com>
2023-07-02 21:57:11 +0500

    .

--- server.js
+++ server.js
@@ -28,7 +28,20 @@ const app = express();
 //     }
 //   })
 // );
-app.use(cors());
+
+const whitelist = ["https://fitness-den.netlify.app"];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+};
+app.use(cors(corsOptions));
+// app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
